259c08bfaf10e85328f05a8cc5102b743dbf099a
leandro caci <leandrocaci299@gmail.com>
2024-11-21 16:30:14 +0100

    add

---
+++ src/components/home/Data.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Data = () => {
+  return (
+    <div className="home__data">
+        <h1 className="home__title">Leandro Caci </h1>
+        <h3 className="home__subtitle">Frontend Developer</h3>
+        <p className="home__description">I'm frontend developer, and i am very passionate and dedicated to my work</p>
+        <a href="#contact" className="button button--flex">Say Hello</a>
+    </div>
+  )
+}
+
+export default Data
